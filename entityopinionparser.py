c2ea27956efbf89a0eaa33088fda362cbb953ee9
Victor Ng <vng@brandeis.edu>
2020-03-04 13:11:10 -0600

    initial commit
    creates a parser to parse XML files. adds the entity aspect in a tuple.

---
+++ entityopinionparser.py
@@ -0,0 +1,37 @@
+import csv
+import xml.etree.ElementTree as ET
+
+def parseXML(xmlfile):
+
+    tree = ET.parse(xmlfile)
+    root = tree.getroot()
+
+    reviews = []
+
+    for sentence in root.iter('sentence'):
+        
+        entity_opinion = {
+            "sentence" : "",
+            "EA_Pair" : []
+        }
+
+        for text in sentence.iter('text'):
+            entity_opinion['sentence'] = text.text
+        
+        try:
+            for category in sentence.iter('Opinion'):
+                opinion = category.get('category')
+                
+                entity = opinion.split('#')[0]
+                attribute = opinion.split('#')[1]
+
+                entity_opinion["EA_Pair"].append((entity, attribute))
+
+        except:
+            print('No opinion')
+    
+        reviews.append(entity_opinion)
+    
+    return reviews
+
+parseXML(r'C:/Users/ujzr76l/Desktop/ABSA/ABSA-15_Laptops_Train_Data.xml')
