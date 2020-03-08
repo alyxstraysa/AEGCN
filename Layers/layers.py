61fd9f98ba8074834108128242f959476b5ff0a6
Victor Ng <vng@brandeis.edu>
2020-03-08 10:48:09 -0500

    adding gcn layer

---
+++ Layers/layers.py
@@ -0,0 +1,28 @@
+import math
+import torch
+from torch.nn.parameter import Parameter
+from torch.nn.modules.module import Module
+
+class GraphConvolution(Module):
+    """
+    Simple GCN layer, similar to https://arxiv.org/abs/1609.02907
+    """
+
+    def __init__(self, in_features, out_features, bias=True):
+        super(GraphConvolution, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.weight = Parameter(torch.FloatTensor(in_features, out_features))
+        if bias:
+            self.bias = Parameter(torch.FloatTensor(out_features))
+        else:
+            self.register_parameter('bias', None)
+        self.reset_parameters()
+
+    def forward(self, input, adj):
+        support = torch.mm(input, self.weight)
+        output = torch.spmm(adj, support)
+        if self.bias is not None:
+            return output + self.bias
+        else:
+            return output
